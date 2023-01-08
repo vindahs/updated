6b427f02c0d31efe8a95833ade100a4d1f3c9c86
vindahs <vincentonoja@ymail.com>
2023-01-08 14:53:02 +0100

    first commit

---
+++ routes/generalRoutes.js
@@ -0,0 +1,303 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Express' });
+});
+
+router.get('/news', function(req, res, next) {
+    res.render('news', { title: 'Express' });
+});
+
+router.get('/projects', function(req, res, next) {
+    res.render('projects', { title: 'Express' });
+});
+
+router.get('/news2', function(req, res, next) {
+    res.render('news2', { title: 'Express' });
+});
+
+router.get('/news3', function(req, res, next) {
+    res.render('news3', { title: 'Express' });
+});
+
+router.get('/news4', function(req, res, next) {
+    res.render('news4', { title: 'Express' });
+});
+
+router.get('/news5', function(req, res, next) {
+    res.render('news5', { title: 'Express' });
+});
+
+router.get('/1', function(req, res, next) {
+    res.render('1', { title: 'Express' });
+});
+
+router.get('/2', function(req, res, next) {
+    res.render('2', { title: 'Express' });
+});
+
+router.get('/3', function(req, res, next) {
+    res.render('3', { title: 'Express' });
+});
+
+router.get('/4', function(req, res, next) {
+    res.render('4', { title: 'Express' });
+});
+
+router.get('/5', function(req, res, next) {
+    res.render('5', { title: 'Express' });
+});
+
+router.get('/6', function(req, res, next) {
+    res.render('6', { title: 'Express' });
+});
+
+router.get('/7', function(req, res, next) {
+    res.render('7', { title: 'Express' });
+});
+
+router.get('/8', function(req, res, next) {
+    res.render('8', { title: 'Express' });
+});
+
+router.get('/9', function(req, res, next) {
+    res.render('9', { title: 'Express' });
+});
+
+router.get('/10', function(req, res, next) {
+    res.render('10', { title: 'Express' });
+});
+
+router.get('/11', function(req, res, next) {
+    res.render('11', { title: 'Express' });
+});
+router.get('/12', function(req, res, next) {
+    res.render('12', { title: 'Express' });
+});
+router.get('/13', function(req, res, next) {
+    res.render('13', { title: 'Express' });
+});
+router.get('/14', function(req, res, next) {
+    res.render('14', { title: 'Express' });
+});
+router.get('/15', function(req, res, next) {
+    res.render('15', { title: 'Express' });
+});
+router.get('/16', function(req, res, next) {
+    res.render('16', { title: 'Express' });
+});
+router.get('/17', function(req, res, next) {
+    res.render('17', { title: 'Express' });
+});
+router.get('/18', function(req, res, next) {
+    res.render('18', { title: 'Express' });
+});
+
+router.get('/44', function(req, res, next) {
+    res.render('44', { title: 'Express' });
+});
+
+router.get('/45', function(req, res, next) {
+    res.render('45', { title: 'Express' });
+});
+
+router.get('/46', function(req, res, next) {
+    res.render('46', { title: 'Express' });
+});
+
+router.get('/47', function(req, res, next) {
+    res.render('47', { title: 'Express' });
+});
+
+router.get('/48', function(req, res, next) {
+    res.render('48', { title: 'Express' });
+});
+
+router.get('/49', function(req, res, next) {
+    res.render('49', { title: 'Express' });
+});
+
+router.get('/50', function(req, res, next) {
+    res.render('50', { title: 'Express' });
+});
+
+router.get('/51', function(req, res, next) {
+    res.render('51', { title: 'Express' });
+});
+
+router.get('/52', function(req, res, next) {
+    res.render('52', { title: 'Express' });
+});
+
+router.get('/53', function(req, res, next) {
+    res.render('53', { title: 'Express' });
+});
+
+router.get('/54', function(req, res, next) {
+    res.render('54', { title: 'Express' });
+});
+
+router.get('/55', function(req, res, next) {
+    res.render('55', { title: 'Express' });
+});
+
+router.get('/56', function(req, res, next) {
+    res.render('56', { title: 'Express' });
+});
+
+router.get('/57', function(req, res, next) {
+    res.render('57', { title: 'Express' });
+});
+
+router.get('/85', function(req, res, next) {
+    res.render('85', { title: 'Express' });
+});
+
+router.get('/208', function(req, res, next) {
+    res.render('208', { title: 'Express' });
+});
+
+router.get('/210', function(req, res, next) {
+    res.render('210', { title: 'Express' });
+});
+
+router.get('/211', function(req, res, next) {
+    res.render('211', { title: 'Express' });
+});
+
+router.get('/39', function(req, res, next) {
+    res.render('39', { title: 'Express' });
+});
+
+router.get('/43', function(req, res, next) {
+    res.render('43', { title: 'Express' });
+});
+
+router.get('/63', function(req, res, next) {
+    res.render('63', { title: 'Express' });
+});
+
+router.get('/70', function(req, res, next) {
+    res.render('70', { title: 'Express' });
+});
+
+router.get('/59', function(req, res, next) {
+    res.render('59', { title: 'Express' });
+});
+
+router.get('/36', function(req, res, next) {
+    res.render('36', { title: 'Express' });
+});
+
+router.get('/77', function(req, res, next) {
+    res.render('77', { title: 'Express' });
+});
+
+router.get('/84', function(req, res, next) {
+    res.render('84', { title: 'Express' });
+});
+
+router.get('/76', function(req, res, next) {
+    res.render('76', { title: 'Express' });
+});
+
+router.get('/73', function(req, res, next) {
+    res.render('73', { title: 'Express' });
+});
+
+router.get('/74', function(req, res, next) {
+    res.render('74', { title: 'Express' });
+});
+
+router.get('/60', function(req, res, next) {
+    res.render('60', { title: 'Express' });
+});
+
+router.get('/55', function(req, res, next) {
+    res.render('55', { title: 'Express' });
+});
+
+router.get('/87', function(req, res, next) {
+    res.render('87', { title: 'Express' });
+});
+
+router.get('/64', function(req, res, next) {
+    res.render('64', { title: 'Express' });
+});
+
+
+router.get('/82', function(req, res, next) {
+    res.render('82', { title: 'Express' });
+});
+
+router.get('/72', function(req, res, next) {
+    res.render('72', { title: 'Express' });
+});
+
+router.get('/62', function(req, res, next) {
+    res.render('62', { title: 'Express' });
+});
+
+router.get('/61', function(req, res, next) {
+    res.render('61', { title: 'Express' });
+});
+
+router.get('/85', function(req, res, next) {
+    res.render('85', { title: 'Express' });
+});
+
+router.get('/64', function(req, res, next) {
+    res.render('64', { title: 'Express' });
+});
+
+router.get('/200', function(req, res, next) {
+    res.render('200', { title: 'Express' });
+});
+
+
+router.get('/201', function(req, res, next) {
+    res.render('201', { title: 'Express' });
+});
+
+router.get('/202', function(req, res, next) {
+    res.render('202', { title: 'Express' });
+});
+
+router.get('/203', function(req, res, next) {
+    res.render('203', { title: 'Express' });
+});
+
+router.get('/204', function(req, res, next) {
+    res.render('204', { title: 'Express' });
+});
+
+router.get('/205', function(req, res, next) {
+    res.render('205', { title: 'Express' });
+});
+
+router.get('/206', function(req, res, next) {
+    res.render('206', { title: 'Express' });
+});
+
+router.get('/207', function(req, res, next) {
+    res.render('207', { title: 'Express' });
+});
+
+router.get('/89', function(req, res, next) {
+    res.render('89', { title: 'Express' });
+});
+
+router.get('/90', function(req, res, next) {
+    res.render('90', { title: 'Express' });
+});
+module.exports = router;
